04468da70515d761345d35268fd651faa685d04b
Deepwarrior <piskun_ya@mail.ru>
2020-01-14 23:09:35 +0200

    Add AIB algorithm

--- src/comb.py
+++ src/comb.py
@@ -4,6 +4,7 @@ import random
 from numpy.linalg import norm
 import numpy as np
 from itertools import combinations
+import math
 
 data_dir = os.path.join(".\\..\\", "data")
 problems = {}
@@ -189,5 +190,71 @@ tabu = Tabu(problem)
 
 
 tabu.set_seed(17)
-tabu.batch_tabu(times = 10)
-print("Best solution", tabu.best_cost)
+#tabu.batch_tabu(times = 10)
+#print("Best solution", tabu.best_cost)
+
+
+
+class Simulated_annealing:
+    def __init__(self, distance, limited_time = 600, T=-1,  cooling_rate=-1, stopping_T=-1):
+        self.distance = distance
+        self.N = len(distance)
+        self.T = math.sqrt(self.N) if T == -1 else T
+        self.T_save = self.T
+        self.cooling_rate = 0.9995 if cooling_rate == -1 else cooling_rate
+        self.stopping_temperature = 1e-8 if stopping_T == -1 else stopping_T
+        self.start_time = None
+        self.limited_time = limited_time
+        self.cur_total_dis = float("inf")
+        self.nodes = [i for i in range(self.N)]
+
+        self.best_tour = None
+        self.best_cost = float("Inf")
+        self.cost_history = []
+
+    def p_accept(self, candidate_cost):
+        return math.exp(-abs(candidate_cost - self.cur_total_dis) / self.T)
+
+    def accept(self, tour):
+        tour_cost = get_total_dist(self, tour)
+        if tour_cost < self.cur_total_dis:
+            self.cur_total_dis, self.cur_tour = tour_cost, tour
+            if tour_cost < self.best_cost:
+                self.best_cost, self.best_tour = tour_cost, tour
+        else:
+            if random.random() < self.p_accept(tour_cost):
+                self.cur_total_dis, self.cur_tour = tour_cost, tour
+
+    def anneal(self):
+        print("Starting annealing.")
+        while self.T >= self.stopping_temperature:
+            next_tour = list(self.cur_tour)
+            [i, l] = random.sample(range(self.N), 2)
+            next_tour[i : (i + l)] = reversed(next_tour[i : (i + l)])
+            self.accept(next_tour)
+            self.T *= self.cooling_rate
+
+        self.cost_history.append((round(time.time() - self.start_time, 2), self.best_cost))
+        print("Best cost obtained: ", self.best_cost)
+        print("best tour", self.best_tour)
+        improvement = 100 * (self.cost_history[1][1] - self.best_cost) / (self.cost_history[1][1])
+        print(f"Improvement over greedy heuristic: {improvement : .2f}%")
+
+    def batch_anneal(self, times = 10):
+        for i in range(1, times + 1):
+            random.seed(17 + i)
+            if time.time() - self.start_time < self.limited_time:
+                print(f"Iteration {i}/{times} -------------------------------")
+                self.T = self.T_save
+                self.cur_tour, self.cur_total_dis = greedy(self)
+                # self.cost_history.append((round(time.time() - self.start_time, 2), self.cur_total_dis))
+                self.anneal()
+
+
+
+
+sa = Simulated_annealing(problem)
+sa.start_time = time.time()
+sa.batch_anneal(times = 10)
+print("Best solution:", sa.best_tour)
+print(time.time() - sa.start_time)
